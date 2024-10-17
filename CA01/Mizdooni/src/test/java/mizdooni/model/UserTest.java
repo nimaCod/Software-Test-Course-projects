331b93bf3086d5c29815f48fda084392cafdae52
nimaCod <t.nima.tajik@gmail.com>
2024-10-17 22:43:14 +0330

    Added testAddReservation For User.java

--- CA01/Mizdooni/src/test/java/mizdooni/model/UserTest.java
+++ CA01/Mizdooni/src/test/java/mizdooni/model/UserTest.java
@@ -1,4 +1,36 @@
 package mizdooni.model;
 
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
 public class UserTest {
+    Reservation reservation = Mockito.mock(Reservation.class);
+    @Mock Address address;
+    @Mock User.Role role;
+    @InjectMocks private User user;
+
+    User create_user(){
+        return new User("nima","password","nima@google.com",address,role);
+    }
+    @BeforeEach
+    void setup(){
+        user = create_user();
+
+    }
+
+    @Test
+    public void testAddReservation(){
+        user.addReservation(reservation);
+        System.out.println(user.getReservations());
+        assertTrue(user.getReservations().contains(reservation));
+        int id = reservation.getReservationNumber();
+        assertEquals(0,id);
+    }
 }
