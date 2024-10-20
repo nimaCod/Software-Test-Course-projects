9a1df85e0f43093906811efdd7d33a303ddff0df
nimaCod <t.nima.tajik@gmail.com>
2024-10-20 23:40:23 +0330

    Completed test for rating.java

--- CA01/Mizdooni/src/test/java/mizdooni/model/RatingTest.java
+++ CA01/Mizdooni/src/test/java/mizdooni/model/RatingTest.java
@@ -9,6 +9,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,13 +26,14 @@ public class RatingTest {
     @Tag("return rounded overall when it is lower than 5")
     @Test
     public void testGetStarCountRoundingOverallLowerThan5(){
-        fail();
+        rating = create_rating(3.5);
+        assertEquals(4,rating.getStarCount());
     }
 
     @Tag("return 5 when overall > 5")
     @Test
     public void testGetStarCountReturning5WhenOverallHigherThan5(){
-        fail();
-    }
+        rating = create_rating(5.5);
+        assertEquals(5,rating.getStarCount());    }
 
 }
